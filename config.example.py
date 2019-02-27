431d2ee1d0c17798ff34a73ea82c0cb879fe366d
Martin Pola <mpola@kth.se>
2019-02-27 11:00:45 +0100

    bättre exempelkonfiguration

--- config.example.py
+++ config.example.py
@@ -3,6 +3,14 @@
 # https://kth.instructure.com/profile/settings
 access_token = ''
 
+# gå in på en kurs i Canvas och titta i adressfältet för att hitta kursens ID
 courses = {
-	'tilpro18': 6135
+	'adk18': 7152,
+	'adk19': 12404,
+	'dasak18': 6156,
+	'tilpro17': 3592,
+	'tilpro18': 6135,
+	'tildav18': 4629,
+	'tildav19': 7489,
+	'ir19': 7518
 }
